05f993e266e6cf484ff657905d1573faf200c984
Harsh Singh <harshsingh16623@gmail.com>
2024-07-25 18:44:23 +0530

    timecomplexity

---
+++ javaprograms/src/timecomplexity.java
@@ -0,0 +1,8 @@
+public class timecomplexity {
+    public static void main(String[] args) {
+        int i, n = 8;
+        for (i = 1; i <= n; i = i * 2) {
+            System.out.println("hellow wolrd");
+        }
+    }
+}
