841d03c2ba4978197ce89ffce76bc3f5b4d9f87c
Harsh Singh <harshsingh16623@gmail.com>
2024-07-26 10:52:13 +0530

    patter

--- javaprograms/src/print5starpatterninvertical.java
+++ javaprograms/src/print5starpatterninvertical.java
@@ -1,4 +1,4 @@
-public class print5starpatterninhorizontal {
+public class print5starpatterninvertical {
     public static void main(String[] args) {
         for(int i=0;i<5;i++)
             System.out.println("*");    }
